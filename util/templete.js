5f454ea919c5de4ea4a7710da27acdbdf2d2a3b6
Ceren Köse <ckcerenck21@gmail.com>
2023-06-30 21:25:50 +0300

    search, dark mode,header,footer

--- util/templete.js
+++ util/templete.js
@@ -1,1 +1,21 @@
-// alert("deneme")
+// Dark mode
+let darkModu=()=>{
+ window.document.classList.toggle("dark_mode")
+}
+
+//input search
+$(document).ready(function(){
+  const searchApi=["Adana","Balikesir","Amasya","Çorum","Denizli","İstanbul","Bolu","Bursa"];
+  $("#tags").autocomplete({
+    source:searchApi
+  })
+});
+
+// Footer
+let newDate=()=>{
+  const date=new Date().getFullYear()
+// document.getElementById("date_id").innerHTML=new Date().getFullYear()
+//jquery DOM 
+$("#date_id").html(date)
+}
+newDate()
