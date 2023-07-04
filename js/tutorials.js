78cac1a6780ce7d82d49ac31697b27962d58c171
Ceren Köse <ckcerenck21@gmail.com>
2023-07-04 19:33:24 +0300

    javascript-1

---
+++ js/tutorials.js
@@ -0,0 +1,9 @@
+alert("deneme");
+console.log("loglama");
+console.log("info");
+console.log("warn");
+console.log("error");
+
+alert("pop-up bilgilendirme-1");
+window.alert("pop-up bilgilendirme-2");
+window.document.writeln("ekranda yazi");
